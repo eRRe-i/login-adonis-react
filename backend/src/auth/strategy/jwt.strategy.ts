dcd118f163a2600cfc6270dffd3ef167bdc0e836
Leandro Bataglia <leandro.bataglia@gmail.com>
2025-01-28 15:15:18 -0300

    [feat] adiciona strategy jwt

---
+++ backend/src/auth/strategy/jwt.strategy.ts
@@ -0,0 +1,37 @@
+import { Injectable } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { PassportStrategy } from "@nestjs/passport";
+import { ExtractJwt, Strategy } from "passport-jwt";
+import { AuthService } from "../auth.service";
+import { AuthDto } from "../dto";
+import { PrismaService } from "src/prisma/prisma.service";
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(
+    Strategy, 
+    'jwt'
+) {
+    constructor(private config: ConfigService, private prisma: PrismaService) {
+        super( {
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            secretOrKey: config.get('JWT_SECRET'),
+        })
+    }
+    async validate(payload: {
+        sub: number,
+        email:string
+    }) {
+        const user = await this.prisma.user.findUnique({
+            where: {
+                id: payload.sub
+            }
+        })
+        if (!user) {
+            return null
+        }
+        else {
+            delete user.hash
+            return user
+        }
+    }
+}
