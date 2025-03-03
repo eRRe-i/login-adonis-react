024053d8770389280f96b8a03f5c191de2b61065
Leandro Bataglia <leandro.bataglia@gmail.com>
2025-03-02 23:33:11 -0300

    feat: implamenta estratégia local para validação de usuário ao logar

--- templates/backend/src/auth/strategy/jwt.strategy.ts
+++ templates/backend/src/auth/strategy/jwt.strategy.ts
@@ -1,35 +1,33 @@
-import { Injectable } from "@nestjs/common";
-import { ConfigService } from "@nestjs/config";
-import { PassportStrategy } from "@nestjs/passport";
-import { ExtractJwt, Strategy } from "passport-jwt";
-import { PrismaService } from "src/prisma/prisma.service";
+import { Injectable } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import { PassportStrategy } from '@nestjs/passport'
+import { ExtractJwt, Strategy } from 'passport-jwt'
+import { PrismaService } from 'src/prisma/prisma.service'
 
 @Injectable()
-export class JwtStrategy extends PassportStrategy(
-    Strategy, 
-    'jwt'
-) {
-    constructor(private config: ConfigService, private prisma: PrismaService) {
-        super( {
-            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-            secretOrKey: config.get('JWT_SECRET'),
-        })
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor(
+    private config: ConfigService,
+    private prisma: PrismaService,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: config.get('JWT_SECRET'),
+    })
+  }
+  async validate(payload: { sub: number; email: string }) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        id: payload.sub,
+      },
+    })
+    if (!user) {
+      console.error('Usuário não encontrado para o payload:', payload)
+      return null
+    } else {
+      delete user.hash
+      console.log('Usuário autenticado via JWT:', user)
+      return user
     }
-    async validate(payload: {
-        sub: number,
-        email:string
-    }) {
-        const user = await this.prisma.user.findUnique({
-            where: {
-                id: payload.sub
-            }
-        })
-        if (!user) {
-            return null
-        }
-        else {
-            delete user.hash
-            return user
-        }
-    }
+  }
 }
